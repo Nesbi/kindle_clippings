3ecf0759d06b2957ede681c5eae450b025d5292d
Nico Balbach <Nico.Balbach@gmail.com>
2020-09-19 14:13:19 +0200

    save book notes

--- kindle.py
+++ kindle.py
@@ -4,28 +4,54 @@ import re
 class Entry:
     def __init__(self,lines):
         self.book = clean_line(lines[0])
-        m = re.search(r"- Ihre Markierung bei Position (.*)-(.*) | Hinzugefügt am (.*)",lines[1])
+        m = re.search(r"- Ihre Markierung bei Position (.*)-(.*) \| Hinzugefügt am (.*)",lines[1])
         self.position = (m.group(1), m.group(2))
         self.timestamp = m.group(3)
         self.text = clean_line("".join(lines[2:]))
+
 
 class Book:
     def __init__(self,title,entries):
         self.title = title
         self.entries = entries
 
+    def get_text(self):
+        return "\n\n".join(e.text + "["+e.position[0]+","+e.position[1]+"]" for e in self.entries)
+
 
 def load_kindle(file_path):
-    with open(file_path, 'r') as clippings:
-        return load_book(load_entries(clippings))
+    delimiter = '==========\n'
 
-def load_entries(clippings):
-    entries = []
-    current_entry = None
-    return [Entry(list(group)) for key, group in groupby(clippings, lambda line: line == '==========\n') if not key]
+    with open(file_path, 'r') as clippings:
+        entries = [Entry(list(group)) 
+                for key, group in
+                groupby(clippings, lambda line: line == delimiter) if not key]
+
+        return [Book(title,clean_entries(list(book_entries))) 
+                for title, book_entries in groupby(entries,lambda entry: entry.book)]
+
+
+def clean_entries(entries):
+    enumerated = enumerate(entries)
+    filtered = [e for (key,e) in enumerated if 
+            len([p for (p_key,p) in entries if p_key != key and entry.text in p.text]) > 0
+
+    filtered_entries = [e for e in entries if not(is_subentry(e,entries))]
+    return sorted(filtered_entries, key=lambda e: [e.position,len(e.text)])
+
+
+def is_subentry(entry_id, entry, entries):
+    parents = [p for p in entries if p.id != entry.id and entry.text in p.text]
+    return len(parents) > 0
+
+
 
 def clean_line(line):
     return line.replace('\ufeff','').strip()
 
-def load_book(entries):
-    return [Book(title,entries) for title, entries in groupby(entries,lambda entry: entry.book)]
+
+def save_book(book,file_path):
+    with open(file_path, 'w+') as notes:
+        notes.write(book.get_text())
+
+
